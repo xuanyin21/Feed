c8bc68f1eee218404c399e8f627ca7377cb26e67
xuanyin21 <yinxuan0201@gmail.com>
2018-03-29 13:14:59 -0700

    Initial Commit

---
+++ Feed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Feed
+//
+//  Created by ACTON Software Develop on 3/29/18.
+//  Copyright © 2018 Shawn Yin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Feed/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Feed
+//
+//  Created by ACTON Software Develop on 3/29/18.
+//  Copyright © 2018 Shawn Yin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
